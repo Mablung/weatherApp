01db5f31877ca643d7c1ed5286e07ef7d5b038af
mablung <berkedemirel61@gmail.com>
2025-06-06 03:24:51 +0300

    Initial Commit

---
+++ weatherApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  weatherApp
+//
+//  Created by Nurettin Berke Demirel on 6.06.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
